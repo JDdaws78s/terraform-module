566f8f21a3d8ab8827179bf06ced765e26c498a8
suresh <sureshkmr123789@gmail.com>
2024-07-20 07:24:12 +0530

    vpc module

---
+++ terraform-vpc-test/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.58.0"
+    }
+  }
+
+    backend "s3" {
+        bucket = "suresh-terraform-remote-state"
+        key    = "ec2-module"
+        region = "us-east-1"
+        dynamodb_table = "remote-locking"
+    }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
