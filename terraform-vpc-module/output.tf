c154a8ff62c59b8f8a64a5714e76b06373530e71
suresh <sureshkmr123789@gmail.com>
2024-07-23 15:04:04 +0530

    vpc-module

--- terraform-vpc-module/output.tf
+++ terraform-vpc-module/output.tf
@@ -9,3 +9,27 @@ output "vpc_id" {
 output "subnet_ids" {
     value = local.merged_tuple
 }
+
+output "public_subnet_ids" {
+    value = aws_subnet.public.id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private.id
+}
+
+output "db_subnet_id" {
+    value = aws_subnet.database.id
+}
+
+output "db_subnet_group_id" {
+    value = aws_db_subnet_group.default.id
+}
+
+output "db_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
+}
+
+output "nat_id" {
+    value = aws_nat_gateway.nat.id
+}
